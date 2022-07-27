6eb60284b3489755533243b374311a2239ba3e2a
YOUNJEI <kyj951211@gmail.com>
2022-07-27 20:56:42 +0900

    update : 백준 - [15651] N과 M (3)

---
+++ 백준/실버3/[15651] N과 M (3)/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> g_arr;
+
+void print(const vector<int> &make) {
+	for (int i = 0; i < make.size(); i++) {
+		cout << make[i] << " ";
+	}
+	cout << "\n";
+}
+
+void dfs(vector<int> make, int N, int M) {
+	if (make.size() == M) {
+		print(make);
+		return ;
+	}
+	for (int i = 0; i < N; i++) {
+		make.push_back(g_arr[i]);
+		dfs(make, N, M);
+		make.pop_back();
+	}
+}
+
+void solve(int N, int M) {
+	vector<int> vi;
+
+	for (int i = 0; i < N; i++) {
+		g_arr.push_back(i + 1);
+	}
+	dfs(vi, N, M);
+}
+
+int main(void) {
+	int N, M;
+
+	cin >> N >> M;
+	solve(N, M);
+	return 0;
+}
